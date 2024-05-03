c6999c3678dc4cf16fb4f91ce881f36c72470a46
tunashred <enachealexandru.traian@gmail.com>
2024-05-03 21:22:01 +0300

    Added initial position and size for windows

--- src/Window.js
+++ src/Window.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import './Window.css';
 import Portfolio from "./Portfolio/Portfolio";
 import Blog from "./Blog/Blog";
 import Contact from "./Contact/Contact";
+import './Window.css';
 
 const Window = ({ id, onClose, pageOpened }) => {
+    const initialSettings = {
+        Portfolio: { position: { x: 100, y: 100 }, size: { width: 1730, height: 810 } },
+        Contact: { position: { x: 1340, y: 50 }, size: { width: 430, height: 620 } },
+        Blog: { position: { x: 130, y: 140 }, size: { width: 1160, height: 780 } }
+    };
+    // Initialize window size and position based on the pageOpened
+    const initialSize = initialSettings[pageOpened]?.size || { width: 500, height: 300 };
+    const initialPosition = initialSettings[pageOpened]?.position || { x: 100, y: 100 };
+    const [windowSize, setWindowSize] = useState(initialSize);
+    const [windowPosition, setWindowPosition] = useState(initialPosition);
+
     const [isDragging, setIsDragging] = useState(false);
     const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 });
-    const [windowPosition, setWindowPosition] = useState({ x: 100, y: 100 }); // Initial position
     const [isResizing, setIsResizing] = useState(false);
     const [resizeOffset, setResizeOffset] = useState({ x: 0, y: 0 });
-    const [windowSize, setWindowSize] = useState({ width: 500, height: 300 }); // Initial size
 
     const handleMouseDown = (e) => {
         const { clientX, clientY } = e;
